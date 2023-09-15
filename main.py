ef228cc286e766de4e893134ec8ecd9fa7eda81c
9OmP <104563841+9OmP@users.noreply.github.com>
2023-09-15 17:39:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from art import logo
+
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n','o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if shift_amount >= 26:
+        shift_amount %= 26
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+print(logo)
+end_caeser = False
+
+while end_caeser != True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    res = str(input("Do you want to keep on using Caeser Cypher?")).lower()
+    if res == 'no':
+        end_caeser = True
+        print("Goodbye!!")
+    elif res == 'yes':
+        end_caeser = False
